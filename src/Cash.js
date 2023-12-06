b9a7f20528f9101f34c3151e2b7fa2da5b452cbb
dlnjtn <delonjutan.dynes@gmail.com>
2023-12-07 03:17:32 +0800

    Menambahkan Result Cash

--- src/Cash.js
+++ src/Cash.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Cash = () => {
+
+    const navigate = useNavigate();
+
+    const goToResultCash=()=>{
+        navigate('/ResultCash');
+    };
+
     return (
         <div className="cash">
             <div className="background">
@@ -14,7 +22,7 @@ export const Cash = () => {
                         <p className="title">Cash</p>
                         <p className="one">To show this code when making a payment at the cashier, use the following code.</p>
                         <p className="two">YOUR CODE: DQH172</p>
-                        <p className="three">Next</p>
+                        <p className="three" onClick={goToResultCash}>Next</p>
                     </div>
                 </div>
             </div>
